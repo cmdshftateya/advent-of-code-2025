5de3f03f4e084349ce5a296daa96353ad11c3b62
Abdul Ateya <aateya@cisco.com>
2025-12-03 14:26:32 -0600

    finished d2p2

--- day2.py
+++ day2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from modulefinder import test
+from re import search
 import sys
 from tracemalloc import start
 
@@ -9,27 +10,19 @@ def clean_data(filename):
     with open(filename, "r") as f:
         data = f.read().strip()
     # Split on commas and drop any empty segments (e.g. from trailing comma)
-    print([segment for segment in data.split(",") if segment])
     return [segment for segment in data.split(",") if segment]
 
 def is_invalid_id(id):
     """
-    An ID is invalid if its decimal representation is composed of some
-    non-empty sequence of digits repeated exactly twice.
-
-    Examples:
-      55      -> "5" repeated twice
-      6464    -> "64" repeated twice
-      123123  -> "123" repeated twice
+    Now, an ID is invalid if it is made only of some sequence of digits repeated at least twice. 
+    So, 12341234 (1234 two times), 123123123 (123 three times), 1212121212 (12 five times), and 1111111 (1 seven times) are all invalid IDs.
     """
-    
-    if len(str(id)) % 2 != 0:
-        return False
-    else:
-        midpoint = len(id) // 2
-        firstpart = id[:midpoint]
-        secondpart = id[midpoint:]
-        return True if firstpart == secondpart else False
+    # if it is repeating, we should able to find the original string in the string doubled without looking at the first character
+    id = str(id)
+    doublestring = id * 2
+    searchstring = doublestring[1:-1]
+    return True if id in searchstring else False
+
 
 def calculate_invalid_id_sum(ranges):
     invalid_id_sum = 0
@@ -40,7 +33,6 @@ def calculate_invalid_id_sum(ranges):
         end_int = int(end_str)
 
         for test_id in range(start_int, end_int):
-            print("ID: ", test_id)
             invalid_id_sum += test_id if is_invalid_id(str(test_id)) else 0
         
     return invalid_id_sum
